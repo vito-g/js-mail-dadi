eae702d45c0afd08e15823b222f085d948907803
Vito Gentile <gentilevito1076@gmail.com>
2020-12-15 15:16:02 +0100

    Start

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var email = prompt ('Inserisci la tua email');
+console.log('La sua email è: ' + email);
+var firstData = document.getElementById('data-uno');
+firstData.innerHTML = 'La sua email è: ' + email;
