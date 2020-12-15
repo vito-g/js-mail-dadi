4b8d102504872f0f437cc1cb04031c1493cd5e6e
Vito Gentile <gentilevito1076@gmail.com>
2020-12-15 16:26:14 +0100

    Analisi

--- js/script.js
+++ js/script.js
@@ -2,3 +2,19 @@ var email = prompt ('Inserisci la tua email');
 console.log('La sua email è: ' + email);
 var firstData = document.getElementById('data-uno');
 firstData.innerHTML = 'La sua email è: ' + email;
+var secondData = document.getElementById('data-due');
+// secondData.innerHTML = 'La sua email è: ' + email;
+
+var lista = ['pippo@gmail.com', 'pluto@yahoo.it', 'paperino@tiscali.it', 'minni@gmail.com', 'paperone@gmail.com', 'qui@gmail.com', 'quo@gmail.com', 'qua@gmail.com'];
+for(var i = 0; i < lista.length; i++) {
+  if (lista[i] === 'pluto@yahoo.it') {
+    secondData.innerHTML = 'Salve utente, siamo felici di rivederla!';
+  }
+  else if (lista[i] !== 'pluto@yahoo.it') {
+    secondData.innerHTML = 'Email errata, accesso negato!';
+  }
+}
+
+  // else if (lista[i] !== 'pluto@yahoo.it') {
+  //     alert('Email errata, accesso negato!');
+  //   }
