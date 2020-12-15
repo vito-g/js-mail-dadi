a8755b6e7a0fc60a42240a17085a19fcf7316e57
Vito Gentile <gentilevito1076@gmail.com>
2020-12-15 16:47:40 +0100

    Correzione

--- js/script.js
+++ js/script.js
@@ -6,14 +6,16 @@ var secondData = document.getElementById('data-due');
 // secondData.innerHTML = 'La sua email è: ' + email;
 
 var lista = ['pippo@gmail.com', 'pluto@yahoo.it', 'paperino@tiscali.it', 'minni@gmail.com', 'paperone@gmail.com', 'qui@gmail.com', 'quo@gmail.com', 'qua@gmail.com'];
+var emailTrovata = false;
 for(var i = 0; i < lista.length; i++) {
-  if (lista[i] === 'pluto@yahoo.it') {
+  if (email === 'pluto@yahoo.it') {
     secondData.innerHTML = 'Salve utente, siamo felici di rivederla!';
   }
-  else if (lista[i] !== 'pluto@yahoo.it') {
+  else if (email !== 'pluto@yahoo.it') {
     secondData.innerHTML = 'Email errata, accesso negato!';
   }
 }
+
 
   // else if (lista[i] !== 'pluto@yahoo.it') {
   //     alert('Email errata, accesso negato!');
