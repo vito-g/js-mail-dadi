a00752e5193814886a165da037a33e0afea87795
Vito Gentile <gentilevito1076@gmail.com>
2020-12-15 20:07:06 +0100

    ESERCIZIO COMPLETATO:  Correzioni e Formattazione

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 var email = prompt ('Inserisci la tua email');
 console.log('La sua email è: ' + email);
-var firstData = document.getElementById('data-uno');
-firstData.innerHTML = 'La sua email è: ' + email;
-var secondData = document.getElementById('data-due');
-// secondData.innerHTML = 'La sua email è: ' + email;
+var data = document.getElementById('advert');
+
+var outputElement = document.getElementById('output');
+outputElement.className = 'denied';
 
 var lista = ['pippo@gmail.com', 'pluto@yahoo.it', 'paperino@tiscali.it', 'minni@gmail.com', 'paperone@gmail.com', 'qui@gmail.com', 'quo@gmail.com', 'qua@gmail.com'];
 var emailTrovata = false;
@@ -13,44 +13,36 @@ for(var i = 0; i < lista.length; i++) {
   }
 }
 
-
-
 if (emailTrovata === true) {
-  alert('Puoi giocare');
-  secondData.innerHTML = 'Puoi giocare a Dadi';
+  alert('Granted Access! Enjoy our Game!');
+  data.innerHTML = 'Granted Access! Enjoy our Game!';
   var min = 1;
   var max = 6;
   var dadoTu = Math.floor(Math.random() * (max + 1 - min) + min);
   var dadoUser = document.getElementById('dado-user');
-  dadoUser.innerHTML = dadoTu;
+  dadoUser.innerHTML = 'Your score: ' + dadoTu;
   var min = 1;
   var max = 6;
   var dadoLui = Math.floor(Math.random() * (max + 1 - min) + min);
   var dadoPc = document.getElementById('dado-pc');
-  dadoPc.innerHTML = dadoLui;
+  dadoPc.innerHTML = 'Pc score: ' + dadoLui;
+  var output = document.getElementById('output');
+
+  if (dadoTu > dadoLui) {
+    var outputElement = document.getElementById('output');
+    outputElement.className = 'granted';
+    output.innerHTML = ('Hai Vinto!');
+  } else if (dadoTu < dadoLui){
+    var outputElement = document.getElementById('output');
+    outputElement.className = 'denied';
+    output.innerHTML = ('Hai Perso!');
+  } else if (dadoTu === dadoLui){
+    var outputElement = document.getElementById('output');
+    outputElement.className = 'draw';
+    output.innerHTML = ('The game is a draw.. Try Again!');
+  }
 }
 else {
-  alert('Non puoi giocare');
-  secondData.innerHTML = 'Non puoi giocare';
+  alert('Denied Access! No Access No Game!');
+  data.innerHTML = 'Denied Access! No Access No Game!';
 }
-
-var output = document.getElementById('output');
-
-if (dadoTu > dadoLui) {
-  output.innerHTML = ('Hai vinto!');
-} else if (dadoTu < dadoLui){
-  output.innerHTML = ('Hai perso!');
-} else {
-  output.innerHTML = ('Partita patta. Riprova!');
-}
-
-
-
-
-
-
-
-
-  // else if (lista[i] !== 'pluto@yahoo.it') {
-  //     alert('Email errata, accesso negato!');
-  //   }
